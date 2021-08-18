6320d07c157c41c6140ca10927af62536e9f638a
tigrtolstoy <tigrtolstoy@gmail.com>
2021-08-18 12:11:32 +0300

    if name == main

--- secure_camera.py
+++ secure_camera.py
@@ -7,37 +7,38 @@ from src.client import send_image
 from src.logger import client_logger
 
 
-CONFIG_FNAME = 'client_config.json'
+if __name__ == '__main__':
+    CONFIG_FNAME = 'client_config.json'
 
-with open(CONFIG_FNAME, 'r') as config_file:
-    config = json.load(config_file)
+    with open(CONFIG_FNAME, 'r') as config_file:
+        config = json.load(config_file)
 
-camera_id = config['camera_id']
-resolution = (config['frame_width'], config['frame_height'])
-camera = Camera(camera_id, resolution)
+    camera_id = config['camera_id']
+    resolution = (config['frame_width'], config['frame_height'])
+    camera = Camera(camera_id, resolution)
 
-while True:
-    img = camera.get_frame()
-    if img is None:
-        client_logger.error(f'Cannot acces to camer by id: [{camera_id}]')
-        break
+    while True:
+        img = camera.get_frame()
+        if img is None:
+            client_logger.error(f'Cannot acces to camer by id: [{camera_id}]')
+            break
 
-    cv2.imshow('secure camera', img)
-    pressed_key = cv2.waitKey(10)
+        cv2.imshow('secure camera', img)
+        pressed_key = cv2.waitKey(10)
 
-    if pressed_key & 0xFF == KEYS.SPACE.value:
-        client_logger.info('Sending image to server...')
-        url = f'http://{config["server_host"]}:{config["server_port"]}/getimage'
+        if pressed_key & 0xFF == KEYS.SPACE.value:
+            client_logger.info('Sending image to server...')
+            url = f'http://{config["server_host"]}:{config["server_port"]}/getimage'
 
-        response = send_image(url, img)
+            response = send_image(url, img)
 
-        if 'error' in response:
-            client_logger.error(response['error'])
-        else:
-            client_logger.info('Server received the image')
+            if 'error' in response:
+                client_logger.error(response['error'])
+            else:
+                client_logger.info('Server received the image')
 
-    elif pressed_key & 0xFF == KEYS.ESC.value:
-        client_logger.info('Camera was stopped')
-        break
+        elif pressed_key & 0xFF == KEYS.ESC.value:
+            client_logger.info('Camera was stopped')
+            break
 
-camera.close()
+    camera.close()
